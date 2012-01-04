c3d976dc900484380da0425f5e1f4735643ff11f
Benjamin W. Smith <benjaminwarfield@just-another.net>
2012-01-04 17:56:30 -0500

    A tad bit of refactoring.

--- spec/StompParseSpec.js
+++ spec/StompParseSpec.js
@@ -1,42 +1,56 @@
 var StompParse = require('../lib/stomp_parse').StompParse;
 
-describe('When parsing a STOMP frame', function() {
-  var stomp_parse;
-  var data;
+describe('When parsing a frame', function() {
 
-  beforeEach(function () {
-    stomp_parse = new StompParse();
-    var data_string = 'CONNECTED\nsession-id:test\n\n\0';
-    var data_string_no_command = 'session-id:test\n\n\0';
+  describe('It will need to parse a command from a buffer', function() {
+    var stomp_parse;
+    var data;
 
-    data = Buffer(data_string, encoding='utf8');
-    data_no_command = Buffer(data_string_no_command, encoding='utf8');
+    beforeEach(function () {
+      stomp_parse = new StompParse();
+      var data_string = 'CONNECTED\nsession-id:test\n\n\0';
+      data = Buffer(data_string, encoding='utf8');
+    });
+
+    it('should return a string when extracting a command', function() {
+      var command = stomp_parse.extract_command(data);
+      expect(command).toEqual('CONNECTED');
+    });
+
+    it('should return an object when extracting headers', function() {
+      var headers = stomp_parse.extract_headers(data);
+      expect(typeof(headers)).toBe('object');
+    });
   });
 
-  it('should return a string when extracting a command', function() {
-    var command = stomp_parse.extract_command(data);
-    expect(command).toEqual('CONNECTED');
+  describe('It will need to parse the headers from the remainder of the buffer', function() {
+    var stomp_parse;
+    var data;
+
+    beforeEach(function () {
+      stomp_parse = new StompParse();
+      var data_string = 'session-id:test\n\n\0';
+      data = Buffer(data_string, encoding='utf8');
+    });
+
+    it('should have length of the headers as a string', function() {
+      var header_data = stomp_parse.extract_headers(data);
+      var expected_header = 'session-id:test';
+      expect(header_data.length).toEqual(expected_header.length);
+    });
+
+    it('should have a key in the header object', function() {
+      var header_data = stomp_parse.extract_headers(data);
+      expect('session-id' in header_data.headers).toBeTruthy();
+    });
+
+    it('should have a value in the header object', function() {
+      var header_data = stomp_parse.extract_headers(data);
+      expect(header_data.headers['session-id']).toEqual('test');
+    });
   });
 
-  it('should return an object when extracting headers', function() {
-    var headers = stomp_parse.extract_headers(data_no_command);
-    expect(typeof(headers)).toBe('object');
+  describe('It will need to parse and return a whole frame', function() {
+    
   });
-
-  it('should have length of the headers as a string', function() {
-    var header_data = stomp_parse.extract_headers(data_no_command);
-    var expected_header = 'session-id:test';
-    expect(header_data.length).toEqual(expected_header.length);
-  });
-
-  it('should have a key in the header object', function() {
-    var header_data = stomp_parse.extract_headers(data_no_command);
-    expect('session-id' in header_data.headers).toBeTruthy();
-  });
-
-  it('should have a value in the header object', function() {
-    var header_data = stomp_parse.extract_headers(data_no_command);
-    expect(header_data.headers['session-id']).toEqual('test');
-  });
-
 });
