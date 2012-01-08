5a943c0c868ac7c1c384830b13ae827ab063f879
Benjamin W. Smith <benjaminwarfield@just-another.net>
2012-01-08 14:33:42 -0500

    Specs and code for parsing a frame off the wire (which is a Buffer).

--- spec/StompParseSpec.js
+++ spec/StompParseSpec.js
@@ -1,4 +1,5 @@
 var StompParse = require('../lib/stomp_parse').StompParse;
+var util = require('util');
 
 describe('When parsing a frame', function() {
 
@@ -52,10 +53,13 @@ describe('When parsing a frame', function() {
 
   describe('It will need to parse and return a whole frame', function() {
     var stomp_parse;
+    var data;
 
     beforeEach(function () {
       stomp_parse = new StompParse();
-      var data_string = 'CONNECTED\nsession-id:test\n\n\0';
+      body = 'test body';
+      content_length = body.length;
+      var data_string = util.format('CONNECTED\nsession-id:test\ncontent-length:%d\n\n%s\0', content_length, body);
       data = Buffer(data_string, encoding='utf8');
     });
 
@@ -64,5 +68,40 @@ describe('When parsing a frame', function() {
       expect(typeof(full_frame)).toBe('object');
     });
 
+    it('should return null if argument is not defined', function() {
+      var null_frame = stomp_parse.parse_frame();
+      expect(null_frame).toBe(null);
+    });
+
+    it('should call StompParse.extract_command with a data buffer', function() {
+      spyOn(stomp_parse, 'extract_command');
+      var full_frame = stomp_parse.parse_frame(data);
+      expect(stomp_parse.extract_command).toHaveBeenCalledWith(data);
+    });
+
+    it('should call StompParse.extract_headers with a data buffer', function() {
+      spyOn(stomp_parse, 'extract_headers');
+      var command = stomp_parse.extract_command(data);
+      var new_data = data.slice(command.length + 1, data.length);
+      var full_frame = stomp_parse.parse_frame(data);
+      expect(stomp_parse.extract_headers).toHaveBeenCalledWith(new_data);
+    });
+
+    it('should return an object named Frame', function() {
+      var full_frame = stomp_parse.parse_frame(data);
+      expect(full_frame.constructor.name).toBe('Frame');
+    });
+
+    it('should have a body in the data', function() {
+      var full_frame = stomp_parse.parse_frame(data);
+      expect('body' in full_frame).toBeTruthy();
+    });
+
+    it('should have "bytes_message" header if "content-length" specified in the headers', function() {
+      var full_frame = stomp_parse.parse_frame(data);
+      expect('bytes_message' in full_frame.headers).toBeTruthy();
+    });
+
   });
+
 });
