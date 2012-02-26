d5271b32cfeba65b7397c8f16d9d9a93f4b8a17d
Benjamin W. Smith <benjaminwarfield@just-another.net>
2012-02-26 17:36:28 -0500

    Add stomp connect, frame handling.

--- spec/StompApiSpec.js
+++ spec/StompApiSpec.js
@@ -103,23 +103,53 @@ describe('StompApi', function() {
   });
 
   describe('When connecting', function() {
-
     it('will call connect on the socket', function() {
       spyOn(stomp, '_setupSocketListeners');
       spyOn(stomp.stompSocket, 'connect');
       stomp.connect();
       expect(stomp.stompSocket.connect).toHaveBeenCalled();
     });
-
   });
-  describe('When disconnecting', function() {
 
+  describe('When disconnecting', function() {
     it('will call disconnect on the socket', function() {
       spyOn(stomp.stompSocket, 'disconnect');
       stomp.disconnect();
       expect(stomp.stompSocket.disconnect).toHaveBeenCalled();
     });
-
   });
 
+  describe('When using send_frame', function() {
+    it('will call stompSocket.write', function() {
+      spyOn(stomp.stompSocket, 'write');
+      stomp.send_frame({as_string: function() {}});
+      expect(stomp.stompSocket.write).toHaveBeenCalled();
+    });
+  });
+  describe('When calling stomp connect', function() {
+    it('will call send_frame()', function() {
+      spyOn(stomp, 'send_frame');
+      stomp.stomp_connect();
+      expect(stomp.send_frame).toHaveBeenCalled();
+    });
+  });
+  describe('When handing a new connect frame', function() {
+    var connected_frame;
+    beforeEach(function() {
+      connected_frame = {
+        command: 'CONNECTED',
+        headers: {
+          'content-length': '0',
+          session: 'd7afec48-b769-4a39-b985-890576a77d8a',
+          bytes_message: true
+        },
+        body: 'content-length:0\nsession:d7afec48-b769-4a39-b985-890576a77d8a\n\n'
+      }
+    });
+    it('will emit a "connected" event', function() {
+      spyOn(stomp, 'emit');
+      stomp.handle_frame(connected_frame);
+      expect(stomp.emit).toHaveBeenCalledWith('connected');
+    });
+  });
 });
